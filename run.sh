6770200dbf0ca7aa4c145455192abae2e7b3ffc3
natesway <64743334+natesway@users.noreply.github.com>
2023-11-02 09:48:33 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+sed -i 's%<string name="CaPath">.*</string>%<string name="CaPath"></string>%' /data/misc/apexdata/com.android.wifi/WifiConfigStore.xml
+sed -i 's%<string name="DomSuffixMatch">.*</string>%<string name="DomSuffixMatch"></string>%' /data/misc/apexdata/com.android.wifi/WifiConfigStore.xml
